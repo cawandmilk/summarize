1bab7f563d035002d59a7276092b7e5695784e5f
myunggyo.oh <cawandmilk@gmail.com>
2022-06-03 21:07:10 +0900

    Update files

---
+++ src/dataset.py
@@ -0,0 +1,144 @@
+import torch
+
+import itertools
+import json
+import logging
+import re
+import tqdm
+
+import numpy as np
+
+from pathlib import Path
+from typing import List
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def get_data(file_dir: List[str], data_type: str):
+    assert data_type in ["book", "paper"]
+    keys = {"input": "input", "label": "label"}
+
+    def _get_book_data(_file_dir: List[str]) -> List[dict]:
+        instances = []
+
+        ## For every folders... (e.g., "기술과학", "기타", "사회과학", and "예술")
+        for file_path in Path(_file_dir).glob("*/*.json"):
+            ## Read json files.
+            with open(file_path, "r", encoding="utf-8") as f:
+                instance = json.load(f)
+
+            ## Renaming and concatenate.
+            instances.append({
+                keys["input"]: instance["passage"],
+                keys["label"]: instance["summary"],
+            })
+
+        return instances
+
+    def _get_paper_data(_file_dir: List[str]) -> List[dict]:
+        instances = []
+
+        ## For every jsons...
+        for file_path in Path(_file_dir).glob("*.json"):
+            ## Read list of json files.
+            with open(file_path, "r", encoding="utf-8") as f:
+                instance = json.load(f)["data"]
+
+            ## Renaming and concatenate.
+            instances.extend([{
+                keys["input"]: instance_["summary_entire"][0]["orginal_text"],
+                keys["label"]: instance_["summary_entire"][0]["summary_text"],
+            } for instance_ in instance])
+            instances.extend([{
+                keys["input"]: instance_["summary_section"][0]["orginal_text"],
+                keys["label"]: instance_["summary_section"][0]["summary_text"],
+            } for instance_ in instance])
+        
+        return instances
+
+    ## Return.
+    return _get_book_data(file_dir) if data_type == "book" else _get_paper_data(file_dir)
+
+
+def make_clean(text):
+    ## Just remove whitespaces as a space token.
+    return re.sub(r"[\s]+", " ", text)
+
+
+class BARTAbstractiveSummarizationDataset(torch.utils.data.Dataset):
+
+    def __init__(
+        self,
+        tokenizer,
+        file_dirs: List[str],
+        max_len: int = 512,
+        ignore_index: int = -100,
+        mode: str = "train",
+    ):
+        super(BARTAbstractiveSummarizationDataset, self).__init__()
+
+        self.tokenizer = tokenizer
+        self.file_dirs = file_dirs
+        self.max_len = max_len
+        self.ignore_index = ignore_index
+        self.mode = mode
+    
+        ## Load raw data.
+        self.raw_data = list(itertools.chain.from_iterable([
+            get_data(file_dir, data_type=Path(file_dir).parts[-2]) for file_dir in file_dirs
+        ]))
+        LOGGER.info(f"[{self.mode}] All raw data loaded: {', '.join(file_dirs)}")
+
+        ## Pre-process data.
+        self.data = [{
+            key: make_clean(value) for key, value in instance.items()
+        } for instance in tqdm.tqdm(self.raw_data, desc="Cleaning", total=len(self.raw_data))]
+        LOGGER.info(f"[{self.mode}] All data cleaned: {', '.join(file_dirs)}")
+
+        ## todo: tokenize using multi-processing.
+        self.data = [{
+            key: np.array(self.tokenizer.encode(value)) for key, value in instance.items()
+        } for instance in tqdm.tqdm(self.data, desc="Tokenizeing", total=len(self.data))]
+        LOGGER.info(f"[{self.mode}] All data tokenized: {', '.join(file_dirs)}")
+
+
+    def _pad(self, text: List[int], token_id: int) -> np.ndarray:
+        ## Pad or truncate.
+        if len(text) < self.max_len:
+            pad = np.array([token_id] * (self.max_len - len(text)))
+            text = np.concatenate([text, pad]) ## pad last
+        else:
+            text = text[:self.max_len] ## truncate last
+
+        return text
+
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+
+    def __getitem__(self, idx):
+        ## Fetch the instance.
+        instance = self.data[idx]
+
+        ## Inputs for encoder.
+        input_ids = self._pad(instance["input"], token_id=self.tokenizer.pad_token_id) ## w/o special tokens
+        attention_mask = (input_ids != self.tokenizer.pad_token_id).astype(float)
+
+        ## Inputs for decoder (generator).
+        decoder_input_ids = np.concatenate([[self.tokenizer.eos_token_id], instance["label"]])
+        decoder_input_ids = self._pad(decoder_input_ids, token_id=self.tokenizer.pad_token_id)
+        decoder_attention_mask = (decoder_input_ids != self.tokenizer.pad_token_id).astype(float)
+
+        ## Target.
+        labels = np.concatenate([instance["label"], [self.tokenizer.eos_token_id]])
+        labels = self._pad(labels, token_id=self.ignore_index)
+
+        return {
+            "input_ids":                torch.from_numpy(input_ids).to(torch.long),
+            "attention_mask":           torch.from_numpy(attention_mask).to(torch.long),
+            "decoder_input_ids":        torch.from_numpy(decoder_input_ids).to(torch.long),
+            "decoder_attention_mask":   torch.from_numpy(decoder_attention_mask).to(torch.long),
+            "labels":                   torch.from_numpy(labels).to(torch.long),
+        }
---
+++ train.py
@@ -0,0 +1,331 @@
+import torch
+import datasets
+import transformers
+
+import argparse
+import datetime
+import logging
+import os
+import pprint
+
+from pathlib import Path
+from typing import List
+
+from src.dataset import BARTAbstractiveSummarizationDataset
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+LOGGER = logging.getLogger(__name__)
+
+
+def define_argparser() -> argparse.Namespace:
+    p = argparse.ArgumentParser()
+
+    p.add_argument(
+        "--train",
+        type=str,
+        nargs="+",
+        default=[
+            "data/book/train", 
+            "data/paper/train", 
+        ],
+        help=" ".join([
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--test",
+        type=str,
+        nargs="+",
+        default=[
+            "data/book/test", 
+            "data/paper/test", 
+        ],
+        help=" ".join([
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--logs",
+        type=str,
+        default="logs",
+        help=" ".join([
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--ckpt",
+        type=str,
+        default="ckpt",
+        help=" ".join([
+            "Default=%(default)s",
+        ]),
+    )
+
+    ## Hyperparameters.
+    p.add_argument(
+        "--pretrained_model_name",
+        type=str,
+        default="gogamza/kobart-base-v1",
+        help=" ".join([
+            "The pretrained model to use.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--n_epochs", 
+        type=int, 
+        default=5,
+        help=" ".join([
+            "The number of iterations of training & validation for the entire dataset.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--warmup_ratio", 
+        type=float, 
+        default=.2,
+        help=" ".join([
+            "The ratio of warm-up iterations that gradulally increase",
+            "compared to the total number of iterations.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--lr", 
+        type=float, 
+        default=5e-5,
+        help=" ".join([
+            "The learning rate.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--weight_decay", 
+        type=float, 
+        default=1e-2,
+        help=" ".join([
+            "Weight decay applied to the AdamW optimizer.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--per_replica_batch_size", 
+        type=int, 
+        default=16,
+        help=" ".join([
+            "If only 1 GPU is available, it is the same value as 'global_batch_size'.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=1,
+        help=" ".join([
+            "Number of updates steps to accumulate the gradients for,",
+            "before performing a backward/update pass.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--use_radam",
+        action="store_false",  ## default: true
+        help=" ".join([
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--inp_max_len", 
+        type=int, 
+        default=512,
+        help=" ".join([
+            "A value for slicing the input data.",
+            "It is important to note that the upper limit is determined",
+            "by the embedding value of the model you want to use.",
+            "Default=%(default)s",
+        ]),
+    )
+    p.add_argument(
+        "--tar_max_len", 
+        type=int, 
+        default=160,
+        help=" ".join([
+            "A value for slicing the output data. It is used for model inference.",
+            "if the value is too small, the summary may be truncated before completion.",
+            "Default=%(default)s",
+        ]),
+    )
+
+    p.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",  ## default: False
+        help=" ".join([
+            "Specifies the debugging mode.",
+            "Default=%(default)s",
+        ]),
+    )
+
+    config = p.parse_args()
+    return config
+
+
+def define_logger(config: argparse.Namespace) -> None:
+    log_format = "[%(asctime)s] [%(levelname)s] %(message)s"
+    level = logging.DEBUG if config.debug else logging.INFO
+
+    ## Save log.
+    logging.basicConfig(level=level, format=log_format)
+
+
+def get_datasets(config, file_dirs: List[str], max_len: int, mode: str = "train"):
+    return BARTAbstractiveSummarizationDataset(
+        tokenizer=transformers.PreTrainedTokenizerFast.from_pretrained(
+            config.pretrained_model_name,
+        ),
+        file_dirs=file_dirs,
+        max_len=max_len,
+        mode=mode,
+    )
+
+
+def get_tokenizer_and_model(config: argparse.Namespace) -> tuple:
+    tokenizer = transformers.PreTrainedTokenizerFast.from_pretrained(config.pretrained_model_name)
+    model = transformers.BartForConditionalGeneration.from_pretrained(config.pretrained_model_name)
+
+    return tokenizer, model
+
+
+# def get_optimizer_and_scheduler(config: argparse.Namespace, model, n_warmup_steps: int, n_total_iterations: int) -> tuple:
+#     if config.use_radam:
+#         optimizer = torch.optim.RAdam(model.parameters(), lr=config.lr)
+#     else:
+#         ## Prepare optimizer and schedule (linear warmup and decay)
+#         no_decay = ["bias", "LayerNorm.weight"]
+#         optimizer_grouped_parameters = [
+#             {
+#                 "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+#                 "weight_decay": 0.01,
+#             },
+#             {
+#                 "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+#                 "weight_decay": 0.0,
+#             }
+#         ]
+
+#         optimizer = torch.optim.AdamW(
+#             optimizer_grouped_parameters,
+#             lr=config.lr,
+#             eps=config.adam_epsilon,
+#         )
+
+#     scheduler = transformers.get_linear_schedule_with_warmup(
+#         optimizer,
+#         n_warmup_steps,
+#         n_total_iterations,
+#     )
+
+#     return optimizer, scheduler
+
+
+def get_metric():
+    ## Get a metric.
+    return datasets.load_metric("rouge")
+
+
+def main(config: argparse.Namespace) -> None:
+    def print_config(config: argparse.Namespace) -> None:
+        pprint.PrettyPrinter(indent=4, sort_dicts=False).pprint(vars(config))
+    print_config(config)
+
+    ## Define logger.
+    define_logger(config)
+
+    ## Get dataset.
+    tr_ds = get_datasets(
+        config, 
+        file_dirs=config.train, 
+        max_len=config.inp_max_len, 
+        mode="train",
+    )
+    ts_ds = get_datasets(
+        config, 
+        file_dirs=config.test, 
+        max_len=config.tar_max_len, 
+        mode="test",
+    )
+
+    ## Get tokenizer and model.
+    tokenizer, model = get_tokenizer_and_model(config)
+
+    ## Path arguments.
+    nowtime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    output_dir = Path(config.ckpt, nowtime)
+    logging_dir = Path(config.logs, nowtime, "run")
+    LOGGER.info(f"Output dir: {output_dir}")
+    LOGGER.info(f"Logging dir: {logging_dir}")
+
+    ## See:
+    ##  - https://huggingface.co/docs/transformers/main_classes/trainer#transformers.Seq2SeqTrainingArguments
+    training_args = transformers.Seq2SeqTrainingArguments(
+        output_dir=output_dir,
+        do_train=True,
+        do_eval=True,
+        do_predict=False,
+        evaluation_strategy="epoch",
+        prediction_loss_only=True,
+        per_device_train_batch_size=config.per_replica_batch_size,
+        per_device_eval_batch_size=config.per_replica_batch_size,
+        gradient_accumulation_steps=config.gradient_accumulation_steps,
+        # eval_accumulation_steps=config.eval_accumulation_steps,
+        learning_rate=config.lr,
+        weight_decay=config.weight_decay,
+        warmup_ratio=config.warmup_ratio,
+        num_train_epochs=config.n_epochs,
+        logging_dir=logging_dir,
+        logging_strategy="steps",
+        logging_steps=10,
+        save_strategy="epoch",
+        # save_steps=1000,
+        fp16=True,
+        dataloader_num_workers=4,
+        disable_tqdm=False,
+        # sharded_ddp="zero_dp_2",
+        load_best_model_at_end=True,
+        generation_max_length=config.tar_max_len,   ## 256
+    )
+
+    ## Define trainer.
+    trainer = transformers.Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tr_ds,
+        eval_dataset=ts_ds,
+        tokenizer=tokenizer,
+        compute_metrics=get_metric(),
+        # optimizers=get_optimizer_and_scheduler(
+        #     config, 
+        #     model, 
+        #     n_warmup_steps=n_warmup_steps,
+        #     n_total_iterations=n_total_iterations,
+        # )
+    )
+
+    ## Train.
+    trainer.train()
+
+    ## Save the best model.
+    # model_dir = list(Path(config.ckpt).glob("*"))[-1]
+
+    # torch.save({
+    #     "config": config,
+    #     "tokenizer": tokenizer,
+    #     "model": trainer.model.state_dict(),
+    # }, Path(model_dir, ".".join([config.model_fpath, "pth"])))
+
+
+if __name__ == "__main__":
+    config = define_argparser()
+    main(config)
